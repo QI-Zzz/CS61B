c294b98e4e3929cc2cb15b4f75c37410d5f97c56
QI-Zzz <zhenqizhao.zzq@gmail.com>
2024-05-01 15:55:06 +0200

    project 1b

---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,119 @@
+public class LinkedListDeque<T> implements Deque<T> {
+
+    private class Node<T> {
+        private T item;
+        private Node<T> pre;
+        private Node<T> next;
+
+        public Node(T item, Node<T> pre, Node<T> next) {
+            this.item = item;
+            this.pre = pre;
+            this.next = next;
+        }
+
+        public  Node(Node<T> pre, Node<T> next) {
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+
+    private Node<T> sentinel;
+    private int size;
+
+
+    public LinkedListDeque() {
+        sentinel = new Node<>(null, null);
+        sentinel.pre = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        Node<T> first = new Node<>(item, sentinel, sentinel.next);
+        sentinel.next.pre = first;
+        sentinel.next = first;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        Node<T> last = new Node<>(item, sentinel.pre, sentinel);
+        sentinel.pre.next = last;
+        sentinel.pre = last;
+        size++;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T item = sentinel.next.item;
+        sentinel.next.next.pre = sentinel;
+        sentinel.next = sentinel.next.next;
+        size--;
+        return item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T item = sentinel.pre.item;
+        sentinel.pre.pre.next = sentinel;
+        sentinel.pre = sentinel.pre.pre;
+        size--;
+        return item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+
+        Node<T> temp = sentinel;
+
+        for (int i = 0; i <= index; i++) {
+            temp = temp.next;
+        }
+        return temp.item;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void printDeque() {
+        Node<T> temp = sentinel.next;
+        while (temp != sentinel) {
+            System.out.println(temp.item);
+            temp = temp.next;
+        }
+    }
+
+    private T getRecursiveHelper(int index, Node<T> p) {
+        if (index == 0) {
+            return p.item;
+        }
+        return getRecursiveHelper(index - 1, p.next);
+    }
+
+    public T getRecursive(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        return getRecursiveHelper(index, sentinel.next);
+    }
+}
